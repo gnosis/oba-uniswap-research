02e1473a36ae92002a6e535471e09d070640125b
Marco Correia <marco.v.correia@gmail.com>
2022-02-16 12:05:44 +0000

    Tests buffers using a mix of new code to get data from dune ... and old code to compute the buffer usage.

--- src/dune_api/queries/dex_ag_trades_within_one_day.sql
+++ src/dune_api/queries/dex_ag_trades_within_one_day.sql
@@ -2,6 +2,6 @@ Select token_a_symbol, token_b_symbol, usd_amount, block_number, token_a_address
 from dex.trades d
 inner join ethereum."transactions" t on d.tx_hash = t.hash
 where category = 'Aggregator' and d.block_time > '2022-02-01 00:00' and d.block_time < '2022-02-03 00:00'
--- and project = 'Gnosis Protocol'
+and project = 'Gnosis Protocol'
 and project != '0x API' -- exluding 0x API because of weird double counting
 and project != 'Matcha' -- exluding matcha because of weird double counting
